4ea39a0a4bf4aafc13dcc842edf22ec3b900c823
Jonathan Otto <jonathan.otto@gmail.com>
2016-07-24 12:27:13 -0700

    Add google analytics (with test account number)

--- ext/src/browser_action/lol.js
+++ ext/src/browser_action/lol.js
@@ -1,5 +1,17 @@
 const dataSourceUrl = 'https://gist.githubusercontent.com/jotto/3ac53d5036bafd9c17fc63ecec6ff9f1/raw';
 
+// GOOGLE ANALYTICS
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-81285902-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 // HELPERS
 
 const getDomain = url => {
